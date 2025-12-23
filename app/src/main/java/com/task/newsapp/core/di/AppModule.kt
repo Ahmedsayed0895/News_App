500acc960bc01acf862d1c82acd4f456ec41c4ae
Ahmed Sayed <ahmedsayed0895@gmail.com>
2025-12-23 11:04:29 +0200

    feat: Add Hilt for dependency injection

---
+++ app/src/main/java/com/task/newsapp/core/di/AppModule.kt
@@ -0,0 +1,43 @@
+package com.task.newsapp.core.di
+
+import android.app.Application
+import androidx.room.Room
+import com.task.newsapp.core.Constants.BASE_URL
+import com.task.newsapp.data.dataSource.local.NewsDataBase
+import com.task.newsapp.data.dataSource.remote.NewsApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import jakarta.inject.Singleton
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+    @Singleton
+    @Provides
+    fun provideNewsApi(): NewsApi = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+        .create(NewsApi::class.java)
+
+    @Provides
+    @Singleton
+    fun providesDAtaBase(app: Application): NewsDataBase =
+        Room.databaseBuilder(
+            app,
+            NewsDataBase::class.java,
+            "new_db.db",
+        ).fallbackToDestructiveMigration(false).build()
+
+    @Singleton
+    @Provides
+    fun providesUserDao(db: NewsDataBase) = db.userDao()
+
+    @Singleton
+    @Provides
+    fun providesArticleDao(db: NewsDataBase) = db.articleDao()
+}
---
+++ app/src/main/java/com/task/newsapp/core/di/RepositoryModule.kt
@@ -0,0 +1,23 @@
+package com.task.newsapp.core.di
+
+import com.task.newsapp.data.repository.AuthRepositoryImpl
+import com.task.newsapp.data.repository.NewsRepositoryImpl
+import com.task.newsapp.domain.repository.AuthRepository
+import com.task.newsapp.domain.repository.NewsRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import jakarta.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+interface RepositoryModule {
+    @Binds
+    @Singleton
+    fun bindAuthRepository(authRepositoryImpl: AuthRepositoryImpl): AuthRepository
+
+    @Binds
+    @Singleton
+    fun bindNewsRepository(newsRepositoryImpl: NewsRepositoryImpl): NewsRepository
+}
