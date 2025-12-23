81756618a1e08141e174acd5d35e2f027265078b
Ahmed Sayed <ahmedsayed0895@gmail.com>
2025-12-23 10:57:21 +0200

    feat: Add app color palette

---
+++ app/src/main/java/com/task/newsapp/ui/theme/Color.kt
@@ -0,0 +1,15 @@
+package com.task.newsapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+
+val White = Color(0xFFFFFFFF)
+val LightGray = Color(0xFFEFF0F3)
+val PrimaryBlue = Color(0xFF1366CA)
+val DarkBlue = Color(0xFF0F121F)
+val BlueGray = Color(0xFF3F4C5F)
+val BorderGray = Color(0xFFD3DEE5)
+val IconsGray = Color(0xFF98A6B5)
+val Gray = Color(0xFF708090)
+val Denim10 = PrimaryBlue.copy(alpha = 0.1f)
+val SkyBlue = Color(0xFF9DE1FE).copy(alpha = 0.1f)
